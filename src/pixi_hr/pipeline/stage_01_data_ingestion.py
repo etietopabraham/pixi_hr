7ba58d09d5781e2ae874888be1bfd45fd3c278d1
eti-etop <etietopdemas@geekradius.com>
2023-08-13 17:11:58 +0300

    optimized data ingestion class

--- src/pixi_hr/pipeline/stage_01_data_ingestion.py
+++ src/pixi_hr/pipeline/stage_01_data_ingestion.py
@@ -3,42 +3,50 @@ from pixi_hr.components.data_ingestion import DataIngestion
 from pixi_hr import logger
 
 
-STAGE_NAME = "Data Ingestion State"
-
 class DataIngestionTrainingPipeline:
-    def __init__(self):
-        pass
-
+    """
+    Pipeline to manage the data ingestion process for training.
+    """
     
+    STAGE_NAME = "Data Ingestion State"
+
     def main(self):
+        """
+        Main execution method for the data ingestion pipeline.
+        """
         try:
+            logger.info("Initializing configuration manager...")
             # Initialize configuration manager to fetch relevant configurations
             config = ConfigurationManager()
             
+            logger.info("Fetching data ingestion configuration...")
             # Fetch the data ingestion-specific configuration
             data_ingestion_config = config.get_data_ingestion_config()
             
+            logger.info("Initializing data ingestion process...")
             # Initialize the data ingestion process with the fetched configuration
             data_ingestion = DataIngestion(config=data_ingestion_config)
             
+            logger.info("Downloading data file...")
             # Download the data file from the specified source
             data_ingestion.download_file()
             
+            logger.info("Extracting ZIP file...")
             # Extract the downloaded ZIP file to the specified directory
             data_ingestion.extract_zip_file()
 
         except Exception as e:
-            # If any error occurs during the process, raise the exception to handle or log it appropriately
-            logger.info(f"An exception {e} has occured")
+            # Log the exception with its traceback
+            logger.exception("An error occurred during the data ingestion process.")
             raise e
 
 
 if __name__ == '__main__':
     try:
-        logger.info(f">>>>>> Stage: {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<< \n\nx==========x")
+        logger.info(f">>>>>> Stage: {DataIngestionTrainingPipeline.STAGE_NAME} started <<<<<<")
+        pipeline = DataIngestionTrainingPipeline()
+        pipeline.main()
+        logger.info(f">>>>>> Stage {DataIngestionTrainingPipeline.STAGE_NAME} completed <<<<<< \n\nx==========x")
     except Exception as e:
-        logger.exception(e)
+        # No need to log the exception here since it's already logged in the main method.
         raise e
